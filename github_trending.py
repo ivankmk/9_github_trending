b1c03692d0142dc0264aeff1edfd25c46107ca36
ivankmk <kumeyko@hotmail.com>
2018-01-27 23:00:11 +0100

    script amended

--- github_trending.py
+++ github_trending.py
@@ -1,9 +1,7 @@
 import requests
 import json
+import collections
 from datetime import timedelta, date
-
-
-TOP_POPULAR = 20
 
 
 def get_week_ago_date():
@@ -11,29 +9,40 @@ def get_week_ago_date():
     return one_week_age
 
 
-def get_trending_repositories():
-    payload = {
-                    'q': 'created:>{}'.format(get_week_ago_date()),
-                    'sort': 'stars',
-                    'order': 'desc'
-                 }
+def get_trending_repositories(top_popular):
+
+    payload = {'q': 'created:>{}'.format(week_ago),
+               'sort': 'stars',
+               'order': 'desc'}
     all_repos = requests.get('https://api.github.com/search/repositories',
                              params=payload)
-    return all_repos.json()['items'][:TOP_POPULAR]
+    return all_repos.json()['items'][:top_popular]
+
+
+def get_issues(owner, repo):
+    issues = requests.get('https://api.github.com/repos/{}/{}/issues'.
+                          format(owner, repo))
+    return len(issues.json())
 
 
 def print_top_repositories(all_repos):
-    rank = 1
-    print('-----------------------------------------------------------')
-    print('\nThis it the TOP {} repositories who created after {}\n'.
-          format(TOP_POPULAR, get_week_ago_date()))
-    for repo in all_repos:
+    delimiter = '-'*60
+    print(delimiter)
+    print('\nThis it the TOP repositories who created after {}\n'.
+          format(week_ago))
+    for rank, repo in enumerate(all_repos, 1):
         print('{}. Repo name: {}'.format(rank, repo['name']))
-        print('         Opened issues: {}'.format(repo['open_issues_count']))
+        print('         Opened issues: {}'.
+              format(get_issues(str(repo['owner']['login']), str(repo['name'])
+                                )
+                     )
+              )
         print('         Link: {}'.format(repo['html_url']))
-        rank += 1
-    print('-----------------------------------------------------------')
+    print(delimiter)
+
 
 if __name__ == '__main__':
-    trending_repos = get_trending_repositories()
+    top_repo = 20
+    week_ago = get_week_ago_date()
+    trending_repos = get_trending_repositories(top_repo)
     print_top_repositories(trending_repos)
